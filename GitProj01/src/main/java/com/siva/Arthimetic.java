3343bd1eed090dc682a14ec7a91b5ce96b27157c
sivap <sivap@SINGAMSIVA>
2024-03-01 11:06:56 +0530

    TL-Commit

--- GitProj01/src/main/java/com/siva/Arthimetic.java
+++ GitProj01/src/main/java/com/siva/Arthimetic.java
@@ -1,7 +1,9 @@
 package com.siva;
 
 public class Arthimetic {
-
+	public int addition(int a, int b) {
+		return a+b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
